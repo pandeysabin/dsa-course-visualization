6f7874b9af547c5224a42097f380bc46e388c7d9
Sabin Pandey <pandeyfcb7849@gmail.com>
2024-08-05 15:12:04 +0545

    add title to the page

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,9 @@
+import { Metadata } from "next";
 import { Home } from "../components/Home";
+
+export const metadata: Metadata = {
+  title: "DSA Course navigation pattern",
+};
 
 export default function Page() {
   return <Home />;
